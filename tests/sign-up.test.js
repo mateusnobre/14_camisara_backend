02f7b20df8067b3190884f1675e5ac6d9e8ae535
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-07-08 11:02:47 -0300

    add all tests for sign-up route

--- tests/sign-up.test.js
+++ tests/sign-up.test.js
@@ -23,6 +23,45 @@ describe("POST /sign-up", () => {
     const status = result.status;
     expect(status).toEqual(201);
   });
+  it("returns 409 for duplicate emails", async () => {
+    const body = {
+      username: testUser.name,
+      password: testUser.password,
+      email: testUser.email,
+    };
+    const result1 = await supertest(app).post("/sign-up").send(body);
+    expect(result1.status).toEqual(201);
+    const result2 = await supertest(app).post("/sign-up").send(body);
+    expect(result2.status).toEqual(409);
+  });
+  it("returns 404 for bad request", async () => {
+    const missingName = {
+      password: testUser.password,
+      email: testUser.email,
+    };
+    const missingNameResult = await supertest(app)
+      .post("/sign-up")
+      .send(missingName);
+    expect(missingNameResult.status).toEqual(404);
+
+    const missingPassword = {
+      username: testUser.name,
+      email: testUser.email,
+    };
+    const missingPasswordResult = await supertest(app)
+      .post("/sign-up")
+      .send(missingPassword);
+    expect(missingPasswordResult.status).toEqual(404);
+
+    const missingEmail = {
+      username: testUser.name,
+      password: testUser.password,
+    };
+    const missingEmailResult = await supertest(app)
+      .post("/sign-up")
+      .send(missingEmail);
+    expect(missingEmailResult.status).toEqual(404);
+  });
 });
 
 afterAll(() => {
