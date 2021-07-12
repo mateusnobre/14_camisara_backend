7c1aaa087f4ea3e9ad46c1c180cc7196d0055da9
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-07-11 21:58:32 -0300

    add test

---
+++ tests/products.test.js
@@ -0,0 +1,19 @@
+import connection from "../src/database.js";
+import app from "../src/ServerApp.js";
+import supertest from "supertest";
+
+//beforeEach(async () => {
+//  await connection.query("DELETE FROM products");
+//});
+
+describe("get /products", () => {
+  it("returns 200 for valid params", async () => {
+    const result = await supertest(app).get("/products");
+    const status = result.status;
+    expect(status).toEqual(200);
+  });
+});
+
+afterAll(() => {
+  connection.end();
+});
