52959ceb7a7b7ec16977e9de292579196b64e2c7
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-07-11 21:59:59 -0300

    fixes merge problems

--- src/ServerApp.js
+++ src/ServerApp.js
@@ -200,4 +200,17 @@ app.post("/evaluation/:product_id", async (req, res) => {
   }
 });
 
+app.get("/products", async (req, res) => {
+  try {
+    const result = await connection.query(
+      `SELECT * 
+      FROM products `
+    );
+    const data = result.rows[0];
+    return res.status(200).send(data);
+  } catch (error) {
+    return res.sendStatus(404);
+  }
+});
+
 export default app;
